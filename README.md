221a2474183ca12bc87957c6a0e3408c69fb026c
Kevin Chen <kevin.chen@veeva.com>
2018-05-09 15:12:21 -0400

    Added files to repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TriviaApp
-Quickly mashed together application for running a Trivia Event
+TriviaApp
+=========
+
+Quickly mashed together application for running a Trivia Event. [Try it now](http://htmlpreview.github.io/?https://github.com/k39chen/TriviaApp/blob/master/index.html)!
+
+![alt='promo.jpg'](promo.jpg)
---
+++ questions.js
@@ -0,0 +1,347 @@
+window.questions = {
+    1: {
+        category: "Sports",
+        color: "#f89728",
+
+        100: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What basketball term derives from a French phrase that circus acrobats used to shout?",
+                choices: [
+                    "Alley-oop",
+                    "Slam Dunk",
+                    "Airball"
+                ]
+            },
+            a: "Alley-oop"
+        },
+        200: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "In 1954, Roger Bannister became the first man to do what under four minutes?",
+                choices: [
+                    "Run a mile",
+                    "Respond to my text",
+                    "Swim 3 laps in an Olympic sized pool"
+                ]
+            },
+            a: "Run a mile",
+            f: "The current world record for fastest mile is held by Hicham El Guerrouj at 3:43.13."
+        },
+        300: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "Legendary NBA player Larry Bird went on to coach what team?",
+                choices: [
+                    "Boston Celtics",
+                    "Indiana Pacers",
+                    "Los Angeles Lakers"
+                ]
+            },
+            a: "Indiana Pacers",
+            f: "Larry Bird played on the Dream Team with Michael Jordan in 1992."
+        },
+        400: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "In baseball, a player is said to get a \"Golden Sombrero\" for doing what?",
+                choices: [
+                    "Breaking a bat",
+                    "Striking out four times",
+                    "Catching ball with hat"
+                ]
+            },
+            a: "Striking out four times",
+            f: "Striking out 5 times is called a Platinum Sombrero and striking out 6 times is a Titanium Sombrero."
+        },
+        500: {
+            timer: 60,
+            q: {
+                type: "text",
+                text: "In sports, referees are often nicknamed after what animal?"
+            },
+            a: "Zebra",
+            f: "Because they often wear black and white stripes."
+        }
+    },
+    2: {
+        category: "Science",
+        color: "#329aea",
+
+        100: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "The longest side of a right triangle is called the what?",
+                choices: [
+                    "Hypotension",
+                    "Hypothalmus",
+                    "Hypotenuse"
+                ]
+            },
+            a: "Hypotenuse",
+            f: "There's nothing fun in math."
+        },
+        200: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What chemical element was observed on the Sun before it was discovered on Earth?",
+                choices: [
+                    "Helium",
+                    "Hydrogen",
+                    "Neon"
+                ]
+            },
+            a: "Helium",
+            f: "The sun uses nuclear fusion to fuse hydrogen nuclei into Helium."
+        },
+        300: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "Which of these countries relies on nuclear power for more than 75% of its electricity?",
+                choices: [
+                    "France",
+                    "Russia",
+                    "Japan"
+                ]
+            },
+            a: "France",
+            f: "They're trying to reduce it to 50% by 2025."
+        },
+        400: {
+            timer: 60,
+            q: {
+                type: "text",
+                text: "Our solar system's main asteroid belt lies between the orbits of which two planets?",
+            },
+            a: "Mars/Jupiter",
+            f: "There are over 7,000 asteroids in the asteroid belt."
+        },
+        500: {
+            timer: 60,
+            q: {
+                type: "text",
+                text: "What was the first planet to be discovered using a telescope?",
+            },
+            a: "Uranus",
+            f: "You can see Mercury, Venus, Mars, Jupiter, and Saturn without a telescope (given the right conditions)."
+        }
+    },
+    3: {
+        category: "Pop Culture",
+        color: "#ef8166",
+
+        100: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What is the name of George R. R. Martin's best-selling fantasy novel series?",
+                choices: [
+                    "A Song of Ice and Fire",
+                    "The Rains of Castamere",
+                    "Game of Thrones"
+                ]
+            },
+            a: "A Song of Ice and Fire",
+            f: "He's written 5 books over 22 years. There are 2 more books still in progress."
+        },
+        200: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "In 2017, what popular Disney movie was remade into a live action movie?",
+                choices: [
+                    "Junglebook",
+                    "Beauty and the Beast",
+                    "Maleficent"
+                ]
+            },
+            a: "Beauty and the Beast",
+            f: "Beauty and the beast stirred a lot of controversy over its LeFou's sexual orientation."
+        },
+        300: {
+            timer: 60,
+            halfAllowed: true,
+            q: {
+                type: "text",
+                text: "Identify the song title and artist.",
+                audioFile: "media/E-T.m4a"
+            },
+            a: "Thinking Out Loud - Ed Sheeran"
+        },
+        400: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "In Psy's viral 2012 song, \"Gangnam Style\", from South Korea, the term \"oppa\" loosely translates to which of these?",
+                choices: [
+                    "Big Time",
+                    "Big Brother",
+                    "Big Spender"
+                ]
+            },
+            a: "Big Brother",
+            f: "Gangnam is an affluent neighbourhood in South Korea. The phrase \"Gangnam Style\" refers to a lifestyle associated with living in the Gangnam district."
+        },
+        500: {
+            timer: 60,
+            halfAllowed: true,
+            q: {
+                type: "text",
+                text: "Identify the song title and artist.",
+                audioFile: "media/Debussy-ClairDeLune.m4a"
+            },
+            a: "Claire de Lune - Claude Debussy"
+        }
+    },
+    4: {
+        category: "History",
+        color: "#6eba83",
+
+        100: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What is the official language of Brazil?",
+                choices: [
+                    "Spanish",
+                    "French",
+                    "Portuguese"
+                ]
+            },
+            a: "Portuguese",
+            f: "Brazil used to be a colony of Portugal."
+        },
+        200: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "Tenzing Norgay is credited as one of the first two men to reach what place?",
+                choices: [
+                    "Top of Mt. Everest",
+                    "North Pole",
+                    "South Pole"
+                ]
+            },
+            a: "Top of Mt. Everest",
+            f: "The first to reach the summit was his partner - Edmund Hillary."
+        },
+        300: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "According to lore, what did the Sphinx ask for before allowing visitors into Thebes?",
+                choices: [
+                    "Offer a gift",
+                    "Pay a toll",
+                    "Answer a riddle"
+                ]
+            },
+            a: "Answer a riddle",
+            f: "A Sphinx has the head of a human and the body of a lion."
+        },
+        400: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "In the Greek epic, \"Homer's Odyssey\", what hero was hidden inside the Trojan Horse?",
+                choices: [
+                    "Hercules",
+                    "Ulysses",
+                    "Achilles"
+                ]
+            },
+            a: "Ulysses",
+            f: "Some Trojan initially wanted to burn the horse but the king was too proud after defeating the Greeks and ordered the wooden horse to be brought inside the city."
+        },
+        500: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "When the US dollar currency was created, what was its value pegged to?",
+                choices: [
+                    "Piece of gold",
+                    "French Franc",
+                    "Spanish Silver Dollar"
+                ]
+            },
+            a: "Spanish Silver Dollar",
+            f: "They moved to the gold standard in 1900 under the Gold Standard Act."
+        }
+    },
+    5: {
+        category: "General",
+        color: "#b95cf7",
+
+        100: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "Which word is spelled the same way in the United States and the United Kingdom?",
+                choices: [
+                    "Television",
+                    "Center",
+                    "Humor"
+                ]
+            },
+            a: "Television"
+        },
+        200: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What kind of tax does NOT apply in the regular edition of Monopoly?",
+                choices: [
+                    "Property Tax",
+                    "Luxury Tax",
+                    "Income Tax"
+                ]
+            },
+            a: "Property Tax",
+            f: "Monopoly was invented to make fun of capitalism but it arguably ended up doing the opposite."
+        },
+        300: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "Which has NOT been the name of an Android operating system?",
+                choices: [
+                    "Jelly Bean",
+                    "Butterscotch",
+                    "Froyo"
+                ]
+            },
+            a: "Butterscotch",
+            f: "The first two versions of android were called Alpha and Beta. The food theme started with version 3: Cupcake."
+        },
+        400: {
+            timer: 60,
+            q: {
+                type: "text",
+                text: "Thanks to an old process for quick copying, architectural plans are traditionally what colour?",
+            },
+            a: "Blue",
+            f: "Blueprints were invented in 1842 by John Herschel."
+        },
+        500: {
+            timer: 60,
+            q: {
+                type: "choice",
+                text: "What is the capital of Australia?",
+                choices: [
+                    "Canberra",
+                    "Melbourne",
+                    "Sydney"
+                ]
+            },
+            a: "Canberra",
+            f: "Citizens of Canberra refer to themselves as \"Canberrans\"."
+        }
+    }
+};
